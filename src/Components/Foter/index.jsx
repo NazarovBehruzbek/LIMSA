89257ba307829d28fba57f9e0b80fae16865dba3
NazarovBehruzbek <behruzbeknazarov001@gmail.com>
2024-04-19 17:02:41 +0500

    new

---
+++ src/Components/Foter/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import React from 'react'
+
+export default function Foter() {
+  return (
+    <div>Foter</div>
+  )
+}
---
+++ src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import React from 'react'
+import Header from './Components/Header/index';
+import { Outlet } from "react-router-dom";
+import Foter from './Components/Foter/index';
+
+export default function Layout() {
+  return (
+    <div>
+        <Header/>
+        <main>
+            <Outlet/>
+        </main>
+        <Foter/>
+    </div>
+  )
+}
